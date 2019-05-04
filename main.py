5f92300a38bd0bcb83bb81b80c3702397e2aa863
Teunis van der Hofstad <teunis@MacBook-Air-van-Teunis.local>
2019-05-05 00:13:47 +0200

    App route /table: flask running

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # https://www.tutorialspoint.com/flask/flask_sqlite.htm
+# from gevent.pywsgi import WSGIServer
+#
+# connection = sqlite3.connect('startupdata.db')
+# print("Database created")
+#
+# connection.execute('CREATE TABLE startups (name TEXT, sector TEXT, bio TEXT)')
+# print("Table startups created")
+# connection.close
 
-def main():
+# cur = connection.cursor()
+# sql_command = 'INSERT INTO startups (name, sector, bio) VALUES (?, ?, ?)'
+# values = ('Company1', 'Tech', 'Hello world')
+# cur.execute(sql_command, values)
+
+@app.route('/table')
+def table():
     connection = sqlite3.connect('startupdata.db')
-    print("Database created")
+    connection.row_factory = sqlite3.Row
 
-    connection.execute('CREATE TABLE startups (name TEXT, sector TEXT, bio TEXT)')
-    print("Table startups created")
-    connection.close
+    cur = connection.cursor()
+    cur.execute('SELECT * FROM startups')
 
-    @app.route('/table')
-    def table():
-        connection = sqlite3.connect('startupdata.db')
-        connection.row_factory = sqlite3.Row
-
-        cur = connection.cursor()
-        cursor.execute('SELECT * FROM startups')
-
-        rows = cur.fetchall();
-        return render_template('table.html', rows = rows)
+    rows = cur.fetchall();
+    return render_template('table.html', rows = rows)
 
 if __name__ == "__main__":
-    main()
+    app.run()
