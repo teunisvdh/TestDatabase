c3250ecaef5ad569d9db9629ce5296a6957ed1f5
Teunis van der Hofstad <teunis@MacBook-Air-van-Teunis.local>
2019-05-05 22:43:53 +0200

    Database working: /home /table First CSS

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
 import sqlite3
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+project_directory = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_directory, "startupdata.db"))
+
 app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+#
+# db = SQLAlchemy(app)
 
 # https://www.tutorialspoint.com/flask/flask_sqlite.htm
 # from gevent.pywsgi import WSGIServer
@@ -16,6 +26,22 @@ app = Flask(__name__)
 # sql_command = 'INSERT INTO startups (name, sector, bio) VALUES (?, ?, ?)'
 # values = ('Company1', 'Tech', 'Hello world')
 # cur.execute(sql_command, values)
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        print(request.form)
+        connection = sqlite3.connect('startupdata.db')
+        print(1)
+        connection.row_factory = sqlite3.Row
+        print(2)
+        cur = connection.cursor()
+        print(3)
+        cur.execute("INSERT INTO startups (name, sector, bio) VALUES(?, ?, ?)",
+        (request.form["Name"], request.form["Sector"], request.form["Bio"]))
+        print(4)
+        connection.commit()
+        print(connection)
+    return render_template('home.html')
 
 @app.route('/table')
 def table():
